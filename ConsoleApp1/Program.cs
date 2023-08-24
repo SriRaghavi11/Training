924cb1ac057154bfd68e5e514c7262c706522f57
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-24 09:47:38 +0530

    LcmGcd Generator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
+﻿using System;
+namespace LcmGcdFinder {
+    class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
+            int a, b, temp, gcd, lcm;
+            Console.WriteLine ("Enter two numbers:");
+            int n1 = int.Parse (Console.ReadLine ());
+            int n2 = int.Parse (Console.ReadLine ());
+            a = n1;
+            b = n2;
+            while (b != 0) {
+                temp = b;
+                b = a % b;
+                a = temp;
+            }
+            gcd = a;
+            lcm = (n1 * n2) / gcd;
+            Console.WriteLine ($"GCD:{gcd}");
+            Console.WriteLine ($"LCM:{lcm}");
+            Console.ReadLine ();
         }
     }
 }
