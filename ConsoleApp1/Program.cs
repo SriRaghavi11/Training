a03d72aca2dd30d525318548c3b085b3dda85fc1
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:08:22 +0530

    Fibonacci Series

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace Fibonacciseries {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter the number of terms:");
+         int number = int.Parse (Console.ReadLine ());
+         int a = 0, b = 1;
+         Console.Write ($"Fibonacci series : {a} {b} ");
+         for (int i = 2; i < number; i++) {
+            int c = a + b;
+            Console.Write ($"{c} ");
+            a = b;
+            b = c;
+         }
+         Console.ReadLine ();
+      }
+   }
 }
