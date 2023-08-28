e534db636c0320883835e0cdec73c7746edb5d3a
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:02:07 +0530

    Number Conversion Game

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace NumberConversionGame {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter the number:");
+         int input = int.Parse (Console.ReadLine ());
+         String Hexadecimal = Convert.ToString (input, 16).ToUpper ();
+         String Binary = Convert.ToString (input, 2);
+         Console.WriteLine ($"Input:{input}");
+         Console.WriteLine ($"HEX:{Hexadecimal}");
+         Console.WriteLine ($"Binary:{Binary}");
+         Console.ReadLine ();
+      }
+   }
 }
+
