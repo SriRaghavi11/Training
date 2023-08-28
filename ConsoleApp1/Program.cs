ced19335a434ad890679cb4639c806cbbd7924b7
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:27:16 +0530

    Multiplication Tables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace MultiplicationTables {
+   class Program {
+      static void Main (string[] args) {
+         for (int i = 1; i <= 10; i++) {
+            Console.WriteLine ($"Multiplication Table for {i}");
+            for (int j = 1; j <= 10; j++) {
+               int result = i * j;
+               Console.WriteLine ($"{i}*{j}={result}");
+            }
+            Console.WriteLine ("\n");
+         }
+         Console.ReadLine ();
+      }
+   }
 }
