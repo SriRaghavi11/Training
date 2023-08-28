c96d53b97f38c1f9745c037a9fb361e0e931241f
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:46:01 +0530

    Pascal's Triangle

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+class Program {
+   static void Main (string[] args) {
+      Console.Write ("Enter the number of rows for the Pascal's triangle: ");
+      int numRows = int.Parse (Console.ReadLine ());
+      for (int i = 0; i < numRows; i++) {
+         int currentValue = 1;
+         Console.Write (new string (' ', numRows - i - 1));
+         for (int j = 0; j <= i; j++) {
+            Console.Write ($"{currentValue} ");
+            currentValue = currentValue * (i - j) / (j + 1);
+         }
+         Console.WriteLine ();
+      }
+   }
 }
