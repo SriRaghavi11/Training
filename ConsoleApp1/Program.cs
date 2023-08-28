26a9888d68d8db63d66119baa7cedc05f6fdff09
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 17:08:21 +0530

    simplified code removing extra lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,8 +5,7 @@ namespace MultiplicationTables {
          for (int i = 1; i <= 10; i++) {
             Console.WriteLine ($"Multiplication Table for {i}");
             for (int j = 1; j <= 10; j++) {
-               int result = i * j;
-               Console.WriteLine ($"{i}*{j}={result}");
+               Console.WriteLine ($"{i}*{j}={i * j}");
             }
             Console.WriteLine ("\n");
          }
