006bd63768c0d5382ddfd17495591fac106cbb79
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:38:12 +0530

    Prime Number Checker

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace Primenumberchecker {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number:");
+         int number = int.Parse (Console.ReadLine ());
+         bool isPrime = true;
+         for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+               isPrime = false; break;
+            }
+         }
+         if (isPrime && number > 1) {
+            Console.WriteLine ($"{number} is Prime");
+         } else { Console.WriteLine ($"{number} is not Prime"); }
+         Console.ReadLine ();
+      }
+   }
 }
