d227486da269c2bbff0c4b559fa98cbfdd1378de
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 14:42:34 +0530

    Palindrome Checker

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,26 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace PalindromeChecker {
+   class Program {
+      static bool IsPalindrome (string input) {
+         input = input.ToLower ().Replace (" ", "").Replace (".", "").Replace (",", "").Replace ("?", "").Replace ("'", "").Replace ("!", "");
+         int left = 0;
+         int right = input.Length - 1;
+         while (left < right) {
+            if (input[left] != input[right])
+               return false;
+            left++;
+            right--;
+         }
+         return true;
+      }
+      static void Main (string[] args) {
+         Console.WriteLine ("Enter a sentence or word:");
+         string input = Console.ReadLine ();
+         if (IsPalindrome (input)) {
+            Console.WriteLine ("It is a palindrome.");
+         } else {
+            Console.WriteLine ("It is not a palindrome.");
+         }
+      }
+   }
 }
