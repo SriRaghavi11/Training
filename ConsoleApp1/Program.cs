748556ed681042927049470facc1fb6ef7fcfa19
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-28 16:35:47 +0530

    string variable added in right format

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,8 @@ namespace NumberConversionGame {
       static void Main (string[] args) {
          Console.Write ("Enter the number:");
          int input = int.Parse (Console.ReadLine ());
-         String Hexadecimal = Convert.ToString (input, 16).ToUpper ();
-         String Binary = Convert.ToString (input, 2);
+         string Hexadecimal = Convert.ToString (input, 16).ToUpper ();
+         string Binary = Convert.ToString (input, 2);
          Console.WriteLine ($"Input:{input}");
          Console.WriteLine ($"HEX:{Hexadecimal}");
          Console.WriteLine ($"Binary:{Binary}");
