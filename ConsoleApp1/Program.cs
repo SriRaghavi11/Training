4bd194632a87da4bf17ca2c6d1b46c0f5e37d17d
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-29 13:52:48 +0530

    added the else statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,13 @@ namespace DigitalRootFinder {
       static void Main (string[] args) {
          Console.Write ("Enter the number:");
          int n = int.Parse (Console.ReadLine ());
-         if (n != 0)
-         {
+         if (n != 0) {
             int digitalroot = 1 + ((n - 1) % 9);
             Console.WriteLine ($"The digital root is {digitalroot}");
+         } else {
+            Console.WriteLine ("The digital root is 0");
          }
-
       }
    }
 }
+
