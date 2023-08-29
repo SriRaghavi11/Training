cc614f5b1f636025655612af6fca3d7f4356ee15
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-29 10:05:13 +0530

    Alignment changes made

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ namespace DigitalRootFinder {
    class Program {
       static void Main (string[] args) {
          Console.Write ("Enter the number:");
-         int n=int.Parse (Console.ReadLine());
+         int n = int.Parse (Console.ReadLine ());
          int digitalroot = n % 9;
          Console.WriteLine ($"The digital root of {n} is {digitalroot}");
       }
