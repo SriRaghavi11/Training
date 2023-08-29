a8cee24a385461c0e4c92cc3e911f3ad6b76f73a
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-29 10:34:56 +0530

    Printing of Diamond

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace PrintDiamond {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Input number of rows (half of the diamond) :");
+         int r = int.Parse (Console.ReadLine ());
+         for (int i = 0; i <= r; i++) {
+            for (int j = 1; j <= r - i; j++)
+               Console.Write (" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+               Console.Write ("*");
+            Console.Write ("\n");
+         }
+         for (int i = r - 1; i >= 1; i--) {
+            for (int j = 1; j <= r - i; j++)
+               Console.Write (" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+               Console.Write ("*");
+            Console.Write ("\n");
+         }
+      }
+   }
 }
