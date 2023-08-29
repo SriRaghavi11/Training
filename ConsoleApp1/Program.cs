cab0989a064da4fcca0d7426d162cfce95be2f85
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-29 10:54:29 +0530

    modified the loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,12 @@ namespace DigitalRootFinder {
       static void Main (string[] args) {
          Console.Write ("Enter the number:");
          int n = int.Parse (Console.ReadLine ());
-         int digitalroot = n % 9;
-         Console.WriteLine ($"The digital root of {n} is {digitalroot}");
+         if (n != 0)
+         {
+            int digitalroot = 1 + ((n - 1) % 9);
+            Console.WriteLine ($"The digital root is {digitalroot}");
+         }
+
       }
    }
 }
