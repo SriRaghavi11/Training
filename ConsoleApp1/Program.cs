e370fcfb8301350630886502662f3e7c87390706
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-29 10:03:40 +0530

    Used Congruence formula from wikipedia to find the digital root

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,21 +2,10 @@
 namespace DigitalRootFinder {
    class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Enter the number:");
-         int number = int.Parse (Console.ReadLine ());
-         int DigitalRoot = CalculateDigitalRoot (number);
-         Console.WriteLine ($"The digital root of {number} is {DigitalRoot}");
-      }
-      static int CalculateDigitalRoot (int num) {
-         while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-               sum += num % 10;
-               num /= 10;
-            }
-            num = sum;
-         }
-         return num;
+         Console.Write ("Enter the number:");
+         int n=int.Parse (Console.ReadLine());
+         int digitalroot = n % 9;
+         Console.WriteLine ($"The digital root of {n} is {digitalroot}");
       }
    }
 }
