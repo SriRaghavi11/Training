9e2b6a0a1103d22ec872b4b7c4a2672254f7ed7e
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-08-30 10:32:05 +0530

    Reduced String Code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace ReduceString {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a string:");
+         string input = Console.ReadLine ();
+         string output = ReducedString (input);
+         Console.WriteLine ("Reduced String:" + output);
+      }
+      static string ReducedString (string input) {
+         System.Text.StringBuilder result = new System.Text.StringBuilder ();
+         foreach (char c in input) {
+            if (result.Length > 0 && result[result.Length - 1] == c) {
+               result.Length--;
+            } else {
+               result.Append (c);
+            }
+         }
+         return result.ToString ();
+      }
+   }
 }
+
+
+
