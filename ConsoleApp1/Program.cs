7c2e41cb60a3e21d938c96cff623a1d8da23bca7
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-01 17:28:16 +0530

    Alignemnets made simpler

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-namespace MultiplicationTables {
+﻿namespace MultiplicationTables {
    class Program {
       static void Main (string[] args) {
          for (int i = 1; i <= 10; i++) {
-            Console.WriteLine ($"Multiplication Table for {i}");
-            for (int j = 1; j <= 10; j++) {
-               Console.WriteLine (String.Format ("{0,2} * {1,2} = {2,2}", i, j, i * j));
-            }
-            Console.WriteLine ("\n");
+            Console.WriteLine ($"Multiplication Table for {i}\n");
+            for (int j = 1; j <= 10; j++) 
+               Console.WriteLine ($"{i,2} * {j,2} = {i * j,2}");
+            Console.WriteLine ();
          }
-         Console.ReadLine ();
       }
    }
 }
