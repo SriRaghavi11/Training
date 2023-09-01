4fdaf99b69f20602f054fd5a3f21ab6a09b435bd
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-01 16:40:41 +0530

    Changed alignment using String.Format

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ namespace MultiplicationTables {
          for (int i = 1; i <= 10; i++) {
             Console.WriteLine ($"Multiplication Table for {i}");
             for (int j = 1; j <= 10; j++) {
-               Console.WriteLine ($"{i}*{j}={i * j}");
+               Console.WriteLine (String.Format ("{0,2} * {1,2} = {2,2}", i, j, i*j));
             }
             Console.WriteLine ("\n");
          }
