ccb674e58ff3cc586614f18963cddf99262cb595
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-01 15:48:59 +0530

    Number guessing game

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace NumberGuessingGame {
+   class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Welcome to the number Guessing game.Find the number I have in mind by guessing it");
+         int guess;
+         int n = new Random ().Next (1, 100);
+         for (int i = 0; ; i++) {
+            Console.Write ("Enter your guess:");
+            bool var = int.TryParse (Console.ReadLine (), out guess);
+            if (var == false) 
+            Console.WriteLine ("Invalid format or number");
+            if (guess < n)
+               Console.WriteLine ("Your guess is low");
+            else if (guess > n)
+               Console.WriteLine ("Your guess is high");
+            else Console.WriteLine ($"Your guess is correct and you got it in {i} tries");
+         }
+      }
+   }
 }
