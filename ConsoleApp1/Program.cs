0ab10b56b65105f418ba6ab84d38cda97ce749e6
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-01 10:06:36 +0530

    Isogram Checker

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace IsogramCheck {
+   class Program {
+      static bool IsIsogram (string word) {
+         word = word.ToLower ();
+         char[] letters = word.ToCharArray ();
+         Array.Sort (letters);
+         for (int i = 1; i < letters.Length; i++) {
+            if (letters[i] == letters[i - 1]) {
+               return false;
+            }
+         }
+         return true;
+      }
+      static void Main (string[] args) {
+         Console.Write ("Enter a word: ");
+         string word= Console.ReadLine ();
+         if (IsIsogram (word)) {
+            Console.WriteLine ("The word is an isogram.");
+         } else {
+            Console.WriteLine ("The word is not an isogram.");
+         }
+      }
+   }
 }
