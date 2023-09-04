6e1d263fa391c4f2aa029ad9ac49f5c619189e46
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-04 15:57:37 +0530

    Modified method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,22 +2,30 @@
 namespace PrintDiamond {
    class Program {
       static void Main (string[] args) {
-         Console.Write ("Input number of rows (half of the diamond) :");
+         Console.Write ("Input number of rows (half of the diamond) : ");
          int r = int.Parse (Console.ReadLine ());
+         PrintDiamondUpperHalf (r);
+         PrintDiamondLowerHalf (r);
+      }
+      static void PrintDiamondUpperHalf (int r) {
          for (int i = 0; i <= r; i++) {
             for (int j = 1; j <= r - i; j++)
                Console.Write (" ");
-            for (int j = 1; j <= 2 * i - 1; j++)
-               Console.Write ("*");
-            Console.Write ("\n");
+            PrintStars (2 * i - 1);
+            Console.WriteLine ();
          }
+      }
+      static void PrintDiamondLowerHalf (int r) {
          for (int i = r - 1; i >= 1; i--) {
             for (int j = 1; j <= r - i; j++)
                Console.Write (" ");
-            for (int j = 1; j <= 2 * i - 1; j++)
-               Console.Write ("*");
-            Console.Write ("\n");
+            PrintStars (2 * i - 1);
+            Console.WriteLine ();
          }
+      }
+      static void PrintStars (int count) {
+         for (int j = 1; j <= count; j++)
+            Console.Write ("*");
       }
    }
 }
