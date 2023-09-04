5fcb98f643f19db19c6ef9b9fc862f68bf55d0c4
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-04 12:08:59 +0530

    changed according to article hints

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
-namespace Primenumberchecker {
+namespace PrimeNumberChecker {
    class Program {
       static void Main (string[] args) {
-         Console.Write ("Enter a number:");
-         int number = int.Parse (Console.ReadLine ());
-         bool isPrime = true;
-         for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-               isPrime = false; break;
+         Console.Write ("Enter a number: ");
+         int n = int.Parse (Console.ReadLine ());
+         bool IsPrime (int number) {
+            if (number <= 1)
+               return false;
+            if (number <= 3)
+               return true;
+            if (number % 2 == 0 || number % 3 == 0)
+               return false;
+            for (int i = 5; i * i <= number; i += 6) {
+               if (number % i == 0 || number % (i + 2) == 0)
+                  return false;
             }
+            return true;
          }
-         if (isPrime && number > 1) {
-            Console.WriteLine ($"{number} is Prime");
-         } else { Console.WriteLine ($"{number} is not Prime"); }
-         Console.ReadLine ();
+         bool isInputPrime = IsPrime (n);
+         if (isInputPrime)
+            Console.WriteLine ($"{n} is a prime number.");
+         else
+            Console.WriteLine ($"{n} is not a prime number.");
       }
    }
 }
+
