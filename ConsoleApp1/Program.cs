8aa07b4016ebb3ebd14663afc50e7a84d6c96081
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-06 16:31:30 +0530

    Code for printing whether or not a number is armstrong

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+namespace ArmstrongNumberFinder {
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number:");
+         int number = int.Parse (Console.ReadLine ());
+         if (isArmstrongNumber (number)) { Console.WriteLine ("It is an armstrong number"); } else { Console.WriteLine ("It is not an armstrong number"); }
+      }
+      private static bool isArmstrongNumber (int num) {
+         int originalNum = num;
+         int sum = 0;
+         int numDigits = num.ToString ().Length;
+         while (num > 0) {
+            int digit = num % 10;
+            sum += (int)Math.Pow (digit, numDigits);
+            num /= 10;
+         }
+         return sum == originalNum;
+      }
+   }
 }
