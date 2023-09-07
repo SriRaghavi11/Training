47300c91130a8a75a584b46838d891515e83a90d
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-07 14:46:43 +0530

    Cose for displaying of two numbers after swappping

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+class Program {
+   static void Main () {
+      Console.Write ("Enter the value of a: ");
+      int a = int.Parse (Console.ReadLine ());
+      Console.Write ("Enter the value of b: ");
+      int b = int.Parse (Console.ReadLine ());
+      (a, b) = (b, a); 
+      Console.WriteLine ($"After swapping, a = {a}, b = {b}");
+   }
 }
