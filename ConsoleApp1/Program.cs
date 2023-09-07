47f77bf296e4c15a71172a513b168c0805706d3e
SriRaghavi11 <sriraghavi.s@metamation.com>
2023-09-07 14:27:17 +0530

    code for finding fraction and integral part digits

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+class Program {
+   static void Main () {
+      Console.Write ("Enter a number: ");
+      string input = Console.ReadLine ();
+      Console.Write ("Integral part: ");
+      bool decimalPointFound = false;
+      foreach (char digit in input) {
+         if (char.IsDigit (digit)) {
+            Console.Write (digit + " ");
+         } else if (digit == '.' && !decimalPointFound) {
+            decimalPointFound = true;
+            Console.WriteLine ();
+            Console.Write ("Fractional part: ");
+         }
+      }
+      if (!decimalPointFound) {
+         Console.WriteLine ("No fractional part.");
+      }
+   }
 }
